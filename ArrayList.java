0e40322f2e5f657fe938707d31ca7ad323ec8b33
Abhishek Charwad <abhishekac@maveric-systems.com>
2022-04-01 10:31:42 +0530

    initial commit

---
+++ ArrayList.java
@@ -0,0 +1,18 @@
+
+
+import java.util.ArrayList;
+
+public class Arraylist
+{
+	public static void main(String args[])
+	{
+		ArrayList al=new ArrayList();
+	
+		al.add(100);
+		al.add(200);
+		al.add(123);
+		
+		System.out.println(al);
+	}
+}
+
